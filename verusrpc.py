08f9954759b357b299e76fea044889b52762d896
jonathan <j@jbsci.org>
2020-09-20 16:59:28 -0700

    Switched to config file and added native SSL

--- verusrpc.py
+++ verusrpc.py
@@ -14,27 +14,44 @@ Written 2020 by Jonathan Barnes <j@jbsci.dev>
 import requests
 from requests.auth import HTTPBasicAuth
 
-#--# Definitions #--#
+#--# Configuration #--#
 
-rpchost = 'http://localhost'
+#modify rpc_api.conf
+class readconfig:
+    '''Reads configuration file and stores relevant information'''
+    def __init__(self):
+        for line in open('rpc_api.conf', 'r'):
+            if line.find('#') >= 0:
+                line = line.split('#')[0]
+                if len(line) > 0:
+                    segments = line.split("=")
+                    inparam  = segments[0].strip()
+                    inval    = segments[1].strip()
+                    if inparam  == 'rpcconf':
+                        if inval[0] == "{" and inval[-1] == "}":
+                            self.rpcconf = ast.literal_eval(inval)
+                        else:
+                            self.rpcconf = inval
+                    elif inparam == 'rpchost':
+                        self.rpchost = inval
 
-#Specifies either path to configuration file or dictionary with configuration information.
-#Default for the verusquery function is use the confpath
-
-confdict = {}
-confpath = ''
+rpcconfig = readconfig()
 
 #---# Functions #---#
 
 def rpcdetails(conffile):
     '''
-    Reads in config file and stores in dictionary for use
+    Reads in config file and stores in dictionary for use.
+
+    If conf is given as a dictionary to start, will just return.
     '''
-    if len(conffile) == 0:
-        print("ERROR: Specify a configuration file with RPC login information")
+    if type(conffile) == dict:
+        return conffile
+    elif len(conffile) == 0:
+        return "ERROR: Specify a configuration file with RPC login information"
     return { line.split('=')[0] : line.split('=')[1].strip('\n') for line in open(conffile, 'r')}
 
-def verusquery(method, params, rpcid= " ", rpcinfo=rpcdetails(confpath), host=rpchost):
+def verusquery(method, params, rpcid= " ", rpcinfo=rpcdetails(rpcconfig.rpcconf), host=rpcconfig.rpchost):
     '''
     Function to query the Verus RPC client. Builds payload based on method and parameters.
     
@@ -43,5 +60,11 @@ def verusquery(method, params, rpcid= " ", rpcinfo=rpcdetails(confpath), host=rp
     if type(params) != list:
         print("ERROR: Params must be given as a list")
     payload = {"jsonrpc" : "1.0", "id": rpcid , "method" : method, "params": params}
-    response = requests.post(host+':'+rpcinfo['rpcport'], json=payload, headers={'content-type': 'text/plain;'}, auth=HTTPBasicAuth(rpcinfo['rpcuser'], rpcinfo['rpcpassword'])).json()
+    response = requests.post(host+':'+ rpcinfo['rpcport'], json=payload, headers={'content-type': 'text/plain;'}, auth=HTTPBasicAuth(rpcinfo['rpcuser'], rpcinfo['rpcpassword'])).json()
     return response
+
+
+#----# Run #----#
+
+if __name__ == '__main__':
+    pass
--- verusverifyapi.py
+++ verusverifyapi.py
@@ -10,7 +10,7 @@ Basic Functionality:
     $host/verifymessage/?message=This is a test message&signer=jbsci@&signature=Af+4EQABQSA1qs5h3yc553W8ulMVU+cVhJgXnkXHeZyEvP7oX9Iiizq3LIY1kWCyrWromhRv7CO1mdViKffFd6jGku0SiCSM
     
 
-Requires verusrpc.py (included in this repo) which needs configured for RPC authentication and host information.
+Requires verusrpc.py (included in this repo) and configuration via rpc_api.conf
 
 Written 2020 by Jonathan Barnes <j@jbsci.dev>
 '''
@@ -20,12 +20,45 @@ Written 2020 by Jonathan Barnes <j@jbsci.dev>
 import verusrpc as vrpc
 from flask import Flask, request
 from flask_api import FlaskAPI, status, exceptions 
+from flask_sslify import SSLify
+
 
 #--# Definitions #--#
 
 app = FlaskAPI(__name__)
 
-#---# Functions #---#
+#---# Configuration #---#
+
+class readconfig:
+    def __init__(self):
+        for line in open('rpc_api.conf', 'r'):
+            if line.find('#') >= 0:
+                line = line.split('#')[0]
+            line = line.strip()
+            if len(line) > 0:
+                segments = line.split('=')
+                inparam = segments[0].strip()
+                inval = segments[1].strip()
+                if inparam == 'apiport':
+                    self.port = int(inval)
+                elif inparam == 'apihost':
+                    self.host = inval
+                elif inparam == 'SSL':
+                    if inval.upper() == 'YES':
+                        self.ssl = True
+                    else:
+                        self.ssl = False
+                elif inparam == 'SSL_KEY':
+                    self.sslkey = inval
+                elif inparam == 'SSL_CRT':
+                    self.sslcrt = inval
+
+apiconf = readconfig()
+if apiconf.ssl:
+    context = (apiconf.sslcrt, apiconf.sslkey)
+    sslify = SSLify(app)
+
+#----# Functions #----#
 
 def verusverify(thing_to_verify, signer, signature, method, rpcid):
     '''
@@ -50,7 +83,7 @@ def verusidentity(identity):
         return result
 
 
-#----# API #----#
+#-----# API #-----#
 
 @app.route("/verifyhash/", methods=["GET"]) 
 def filehash():
@@ -100,7 +133,11 @@ def getid():
 
 
 
-#-----# Run #-----#
+#------# Run #------#
 
 if __name__ == '__main__':
-    app.run()
+    if apiconf.ssl:
+        context = (apiconf.sslcrt, apiconf.sslkey)
+        app.run(host=apiconf.host, port=apiconf.port, ssl_context = context)
+    else:
+        app.run(host=apiconf.host, port=apiconf.port)
